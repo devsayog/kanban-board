7b57de009a87c5e1e6154d6c88e1a98726ca5829
devsayog <devsayog@gmail.com>
2022-08-27 13:18:19 +0545

    add dark/light mode

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,9 +7,9 @@ type LayoutProps = {
 }
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <section className="min-h-screen w-full bg-gray-light-1">
+    <section className="min-h-screen w-full bg-gray-light-1 dark:bg-slate-dark">
       <Header />
-      <main className="container mx-auto mt-16 font-inter">{children}</main>
+      <main className="container mx-auto pt-16 font-inter">{children}</main>
     </section>
   )
 }
