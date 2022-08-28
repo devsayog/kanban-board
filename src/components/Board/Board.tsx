bc6cfdaa847f2a7928bb28bc30104dc2b9f3a7d3
devsayog <devsayog@gmail.com>
2022-08-28 22:36:14 +0545

    dispaly modal with form

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,22 +1,37 @@
 import { board } from '@/data'
+import useBoolean from '@/hooks/useBoolean'
 
 import Column from '../Column'
+import AppModal from '../common/AppModal'
 import Button from '../common/Button'
+import Form from '../common/Form'
+import { Heading3 } from '../common/Typography'
 
 const Board = () => {
+  const [value, { setFalse, setTrue }] = useBoolean(false)
   return (
-    <section className="flex space-x-6 overflow-x-auto py-4">
-      {board.map((b, i) => (
-        <Column
-          index={i}
-          key={b.id}
-          id={b.id}
-          title={b.title}
-          tasks={b.tasks}
+    <>
+      <section className="flex space-x-6 overflow-x-auto py-4">
+        {board.map((b, i) => (
+          <Column
+            index={i}
+            key={b.id}
+            id={b.id}
+            title={b.title}
+            tasks={b.tasks}
+          />
+        ))}
+        <Button click={setTrue} text="create new board" />
+      </section>
+      <AppModal value={value} setFalse={setFalse}>
+        <Heading3 text="Create new board" />
+        <Form
+          submit={console.log}
+          btnText="add new board"
+          placeholder="your new board ..."
         />
-      ))}
-      <Button click={() => {}} text="create new board" />
-    </section>
+      </AppModal>
+    </>
   )
 }
 
