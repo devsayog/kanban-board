4e90d26b359295f3bc153608e97be18d5949857a
devsayog <devsayog@gmail.com>
2022-08-28 18:15:52 +0545

    add @tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
@@ -29,5 +31,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
